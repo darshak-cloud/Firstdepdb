f8fd6e35e1c6b99455c066e06cc5ce6a1aa9f120
darshak-cloud <140959544+darshak-cloud@users.noreply.github.com>
2026-01-06 08:01:14 +0530

    Commeted by darshak

--- FirstPro/Firstapp/views.py
+++ FirstPro/Firstapp/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
+    print("This is first view function")
     st="<h1>Welcome to our first appilcation</h1>"
     return HttpResponse(st)
